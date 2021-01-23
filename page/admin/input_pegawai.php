98ad3e882bbacfd71cb8bc6e709abdcb01791d98
kb0b <rgiovanni17@gmail.com>
2021-01-23 21:41:02 +0700

    form input pegawai

--- page/admin/input_pegawai.php
+++ page/admin/input_pegawai.php
@@ -55,7 +55,25 @@
     </nav>
 
     <div class="container">
-        
+        <div class="col text-center mt-4">
+            <h3>Input Pegawai</h3>
+        </div>
+        <br>
+        <form>
+            <div class="mb-3">
+                <label for="nama" class="form-label">Name</label>
+                <input type="text" class="form-control" id="name" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="username" class="form-label">User Name</label>
+                <input type="text" class="form-control" id="username" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
     </div>
 
     <script src="../../js/all.js"></script>
